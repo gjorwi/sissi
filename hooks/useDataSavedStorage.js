3baf686d81f53a461a1263e2bb0a1e66698b812a
jorwi jesus <jgjg.0503@gmail.com>
2024-02-15 10:09:21 -0400

    hook

---
+++ hooks/useDataSavedStorage.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+
+export default function useGetSessionStorage(valName){
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    setValue(sessionStorage.getItem(valName))
+    // alert(sessionStorage.getItem(valName))
+  }, [])
+
+  return {value}
+}
